7f59524919e99ea18e9c4e04c2d5da78f0f67a94
Dnyn Shinde <shindekartik811@gmail.com>
2020-10-25 22:26:03 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const app = express();
 // connect Databse
 connectDB();
 
-const port = process.env.PORT;
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 
@@ -15,6 +13,19 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+// Setup express port
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`serer is up on running ${port}`);
